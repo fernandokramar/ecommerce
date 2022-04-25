ecf21d43ea7aa752757e105679b3f037c713685b
Fernando Kramar <macedokramar@gmail.com>
2022-04-25 14:37:30 -0300

    removing commented code

--- ecommerce_adm/src/services/auth/loggedin.js
+++ ecommerce_adm/src/services/auth/loggedin.js
@@ -1,13 +1,10 @@
 module.exports = (req, res, next) => {
 
 	console.log('service >>>>>>>>>>>>>>>>>>> auth.loggedin.js')
-	//console.log(req);
 
-	// if (req.user) {
-	// 	return next()
-	// }
+	if (req.user) {
+		return next()
+	}
 
-	// return res.redirect('/auth')
-
-	return next()
+	return res.redirect('/auth')
 }
